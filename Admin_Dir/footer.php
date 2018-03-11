a1cf105e48114c6fffbe4fa1f22ab1d82ba37093
MRPartik <loyolapat04@gmail.com>
2018-03-11 17:31:36 +0800

    relocate files, fixing coding

---
+++ Admin_Dir/footer.php
@@ -0,0 +1,39 @@
+<script src="../js/jquery.js"></script>
+<script src="../js/jquery-ui/jquery-ui-1.10.1.custom.min.js"></script>
+<script src="../bs3/js/bootstrap.min.js"></script>
+<script src="../js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="../js/jquery.scrollTo.min.js"></script>
+<script src="../js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
+<script src="../js/jquery.nicescroll.js">
+</script>
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
+<script src="../js/skycons/skycons.js"></script>
+<script src="../js/jquery.scrollTo/jquery.scrollTo.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script type="text/javascript" src="../js/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"></script>
+<script src="../js/jquery-tags-input/jquery.tagsinput.js"></script>
+<script src="../js/calendar/clndr.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+<script src="../js/calendar/moment-2.2.1.js"></script>
+<script src="../js/evnt.calendar.init.js"></script>
+<script src="../js/jvector-map/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="../js/jvector-map/jquery-jvectormap-us-lcc-en.js"></script>
+<script src="../js/select2/select2.js"></script>
+<script src="../js/select-init.js"></script>
+<script src="../js/gauge/gauge.js"></script>
+<!--clock init-->
+<script src="../js/css3clock/js/css3clock.js"></script>
+<!--Easy Pie Chart-->
+<script src="../js/easypiechart/jquery.easypiechart.js"></script>
+<!--Sparkline Chart-->
+<script src="../js/sparkline/jquery.sparkline.js"></script>
+<script src="../js/jquery.customSelect.min.js"></script>
+<!--sweet alert -->
+<script src="../js/sweetalert/sweetalert-dev.js"></script>
+<script src="../js/sweetalert/sweetalert.min.js"></script>
+<!--common script init for all pages-->
+<script src="../js/scripts.js"></script>
+<!--script for this page-->
+<script src="../js/advanced-form.js"></script>
+<script type="text/javascript" src="../js/fuelux/js/spinner.min.js"></script>
+<script src="../js/advanced-datatable/js/autocomplete.js"></script>
