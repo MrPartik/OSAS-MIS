210cb9d31316e4a2d2972daf9cb814430902f56d
MRPartik <loyolapat04@gmail.com>
2018-03-16 14:45:50 +0800

    fix codee

--- OSAS_Dir/studClearanceSem.php
+++ OSAS_Dir/studClearanceSem.php
@@ -184,13 +184,10 @@ include('../config/connection.php');
             <!-- Placed js at the end of the document so the pages load faster -->
             <!--Core js-->
             <?php include('footer.php')?>
-                <script type="text/javascript " language="javascript " src="../js/advanced-datatable/js/jquery.dataTables.js "></script>
-                <script type="text/javascript " src="../js/data-tables/DT_bootstrap.js "></script>
-                <script src="../js/dynamic_table_init.js "></script>
     </body>
 
 </html>
-<script>
+<script>   
     $(document).ready(function () {
         var dataSrc = [];
         var table = $('#dynamic-table').DataTable({
@@ -217,7 +214,7 @@ include('../config/connection.php');
     $("#TableStudSanc ").on("click ", "#StudSemModalClick ", function () {
         var datas = $(this).attr("value");
         $.ajax({
-            url: "studSanctionModal.php?StudNo=" + datas
+            url: "studClearanceSemModal.php?StudNo=" + datas
             , cache: false
             , async: false
             , success: function (result) {
