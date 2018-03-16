d241d413b1e45ee2423692f810941bce2d6e8c97
MRPartik <loyolapat04@gmail.com>
2018-03-17 04:28:25 +0800

    fix code

--- Admin_Dir/header.php
+++ Admin_Dir/header.php
@@ -14,27 +14,6 @@ $user_check = $_SESSION['logged_user']['username']; ?>
 <!DOCTYPE html>
 
 <head>
-<<<<<<< HEAD
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="ThemeBucket">
-        <link rel="shortcut icon" href="../images/favicon.png">
-        <link rel="stylesheet" type="text/css" href="../js/select2/select2.css" />
-        <link href="../bs3/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../js/jquery-ui/jquery-ui-1.10.1.custom.min.css" rel="stylesheet">
-        <link href="../css/bootstrap-reset.css" rel="stylesheet">
-        <link href="../font-awesome/css/font-awesome.css" rel="stylesheet">
-        <link href="../js/jvector-map/jquery-jvectormap-1.2.2.css" rel="stylesheet">
-        <link href="../css/clndr.css" rel="stylesheet">
-        <link href="../js/css3clock/css/style.css" rel="stylesheet">
-        <link rel="stylesheet" href="../js/morris-chart/morris.css">
-        <link href="../css/style.css" rel="stylesheet">
-        <link href="../css/style-responsive.css" rel="stylesheet" />
-        <link href="../js/sweetalert/sweetalert.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="../js/select2/select2.css" />
-        <link rel="stylesheet" type="text/css" href="../js/jquery-tags-input/jquery.tagsinput.css" />
-=======
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
@@ -54,17 +33,12 @@ $user_check = $_SESSION['logged_user']['username']; ?>
     <link href="../js/sweetalert/sweetalert.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../js/select2/select2.css" />
     <link rel="stylesheet" type="text/css" href="../js/jquery-tags-input/jquery.tagsinput.css" />
->>>>>>> ea5d794... updated na yung js(dinagdagan ko tas inalis yung alert) dun sa may adminmedyo okay na yung members and position
     <link rel="stylesheet" type="text/css" href="../js/bootstrap-datepicker/css/datepicker.css" />
 
     <link href="../js/advanced-datatable/css/demo_page.css" rel="stylesheet" />
     <link href="../js/advanced-datatable/css/demo_table.css" rel="stylesheet" />
     <link rel="stylesheet" href="../js/data-tables/DT_bootstrap.css" />
 </head>
-<<<<<<< HEAD
-
-=======
->>>>>>> ea5d794... updated na yung js(dinagdagan ko tas inalis yung alert) dun sa may adminmedyo okay na yung members and position
 
 
 <section id="container">
