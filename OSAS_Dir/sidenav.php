714a8a3bc5c4d1c73c4dc03dba95161398b843c9
MRPartik <loyolapat04@gmail.com>
2018-03-16 18:13:28 +0800

    adding

--- OSAS_Dir/sidenav.php
+++ OSAS_Dir/sidenav.php
@@ -16,12 +16,11 @@
                         <li <?php if( $currentPage==='OSAS_StudSanction' ) { echo 'class="active"';} ?>><a href="studSanction.php">Student Sanction</a></li>
                     </ul>
                 </li>
-                <li class="sub-menu">
-                    <a href="javascript:;" <?php if( $currentPage==='OSAS_OrgAccreditation' || $currentPage==='OSAS_OrgApplication' || $currentPage==='OSAS_OrgCompliance' ) { echo 'class="active"';} ?> > <i class="fa fa-users"></i> <span>Organization Management</span> </a>
+                <li class="sub-menu"> <a href="javascript:;" <?php if( $currentPage==='OSAS_OrgAccreditation' || $currentPage==='OSAS_OrgApplication' || $currentPage==='OSAS_OrgCompliance' ) { echo 'class="active"';} ?> > <i class="fa fa-users"></i> <span>Organization Management</span> </a>
                     <ul class="sub">
-                        <li <?php if( $currentPage==='OSAS_OrgApplication' ) { echo 'class="active"';} ?>  ><a href="OrganizationApplication.php">Applicants</a></li>
-                        <li <?php if( $currentPage==='OSAS_OrgCompliance' ) { echo 'class="active"';} ?> ><a  href="OrganizationCompliance.php">Organization Profile</a></li>
-                        <li  <?php if( $currentPage==='OSAS_OrgAccreditation' ) { echo 'class="active"';} ?>  ><a href="OrganizationAccreditation.php">Accreditation</a></li>
+                        <li <?php if( $currentPage==='OSAS_OrgApplication' ) { echo 'class="active"';} ?> ><a href="OrganizationApplication.php">Applicants</a></li>
+                        <li <?php if( $currentPage==='OSAS_OrgCompliance' ) { echo 'class="active"';} ?> ><a href="OrganizationCompliance.php">Organization Profile</a></li>
+                        <li <?php if( $currentPage==='OSAS_OrgAccreditation' ) { echo 'class="active"';} ?> ><a href="OrganizationAccreditation.php">Accreditation</a></li>
                         <li><a href="#">Financial Statement</a></li>
                         <li><a href="#">Voucher</a></li>
                         <li><a href="#">Remittance</a></li>
@@ -31,20 +30,15 @@
                         <i class="fa fa-money"></i>
                         <span>Financial Assistance</span>
                     </a> </li>
-                <li>
-                    <a <?php if( $currentPage==='OSAS_LossID' ) {echo 'class="active"';} ?> href="LossIDRegicard.php"> <i class="fa fa-asterisk"></i> <span>Loss of ID and Regi Card</span> </a>
-                </li>
-                <li class="sub-menu">
-                    <a  <?php if( $currentPage==='OSAS_StudClearance' ) { echo 'class="active"';}?>  href="javascript:;" > <i class="fa fa-tag"></i> <span>Clearance Management</span> </a>
+                <li> <a <?php if( $currentPage==='OSAS_LossID' ) {echo 'class="active"';} ?> href="LossIDRegicard.php"> <i class="fa fa-asterisk"></i> <span>Loss of ID and Regi Card</span> </a> </li>
+                <li class="sub-menu"> <a <?php if( $currentPage==='OSAS_StudClearance' ) { echo 'class="active"';}?>  href="javascript:;" > <i class="fa fa-tag"></i> <span>Clearance Management</span> </a>
                     <ul class="sub">
-                        <li  <?php if( $currentPage==='OSAS_StudClearance' ) { echo 'class="active"';} ?> ><a href="studClearanceSem.php" >Semestral Clearance</a></li>
-                        <li><a href="#" >General Clearance</a></li>
+                        <li <?php if( $currentPage==='OSAS_StudClearance' ) { echo 'class="active"';} ?> ><a href="studClearanceSem.php">Semestral Clearance</a></li>
+                        <li><a href="#">General Clearance</a></li>
                     </ul>
                 </li>
-                <li class="sub-menu" >
-                    <a href="docuArchiving.php" <?php if( $currentPage==='OSAS_docuArchive' ) {echo 'class="active"';} ?> > <i class="fa fa-envelope"></i> <span>Document Archiving </span> </a>
-                </li>
-                <li class="sub-menu-bottom">
+                <li class="sub-menu"> <a href="docuArchiving.php" <?php if( $currentPage==='OSAS_docuArchive' ) {echo 'class="active"';} ?> > <i class="fa fa-envelope"></i> <span>Document Archiving </span> </a> </li>
+                <li>
                     <a href="#admin-login" data-toggle="modal"> <i class="fa fa-unlock"></i> <span>Switch Administrator Account </span> </a>
                 </li>
             </ul>
@@ -54,7 +48,7 @@
 </aside>
 <!-- Modal -->
 <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="admin-login" class="modal fade">
-    <div class="modal-dialog">
+    <div class="modal-dialog" style="width:500px">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
