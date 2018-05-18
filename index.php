3f01cb6f085723e0e96e483012f577761ef39ef8
OSAS-HEAD <osasmis1104@gmail.com>
2018-05-18 15:51:28 +0800

    Pending for portal

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php include("header.php")?>
+
+  <body class="body-500">
+
+    <div class="error-head"> </div>
+
+    <div class="container ">
+
+      <section class="error-wrapper text-center">
+          <h1><img src="images/500.png" alt=""></h1>
+          <div class="error-desk">
+              <h2>Under maintenance<h2>
+              <p class="nrml-txt-alt">**********</p>
+              <p>Thank you for your patience <label style="color:#FF5252;font-weight:bold">:)</label></p>
+          </div>
+          <a href="index.php" class="back-btn"><i class="fa fa-home"></i> Back To Home</a>
+      </section>
+
+    </div>
+
+
+  </body>
 <?php include("footer.php")?>
