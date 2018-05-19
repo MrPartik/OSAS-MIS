3ca86710804f0d35410a4e51deddad6bf6a6b95c
OSAS-HEAD <osasmis1104@gmail.com>
2018-05-19 14:58:44 +0800

    Fix

--- DIR_ADMIN/sidenav.php
+++ DIR_ADMIN/sidenav.php
@@ -28,8 +28,8 @@
                     <ul class="sub">
                         <li <?php if( $currentPage==='Admin_AccrReq' ) { echo 'class="active"';} ?> ><a href="AccreditationRequirement.php">Accreditation Requirement</a></li>
                         <li <?php if( $currentPage==='Admin_Course' ) { echo 'class="active"';} ?>><a href="Course.php">Course</a></li>
+                        <li <?php if( $currentPage==='Admin_OrgCat' ) { echo 'class="active"';} ?>><a href="AcademicCategory.php">Organization Category</a></li>
                         <li <?php if( $currentPage==='Admin_NonAcad' ) { echo 'class="active"';} ?>><a href="NonAcademicCategory.php">Non Academic Category </a></li>
-                        <li <?php if( $currentPage==='Admin_OrgCat' ) { echo 'class="active"';} ?>><a href="AcademicCategory.php">Academic Category</a></li>
                     </ul>
                 </li>
                 <li class="sub-menu">
